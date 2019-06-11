877f7f781a265c881c0ce024caab25b91572dfdc
nagyb1996 <nagyb@wit.edu>
2019-06-11 17:53:04 -0400

    add comments to tip calculator java files

--- TipsCalculator/app/src/main/java/com/example/tipscalculator/Calculator.java
+++ TipsCalculator/app/src/main/java/com/example/tipscalculator/Calculator.java
@@ -13,97 +13,115 @@ import android.widget.EditText;
 
 public class Calculator extends AppCompatActivity {
 
-    private double tip_per_person_adjusted;
-    private double tip_per_person_unadjusted;
+    private double tip_per_person_adjusted; // store value of calculated adjusted tip
+    private double tip_per_person_unadjusted; // store value of calculated unadjusted tip
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
-        Button calculator_button = findViewById(R.id.calculate_button);
+        Button calculator_button = findViewById(R.id.calculate_button); // find calculate button by id
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
+        // behavior when calculate button is clicked
         calculator_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.v("myApp", "Calculator Calculate button is clicked");
 
+                // EditText input for bill total, tip percent, and number of people
                 EditText bill_input = findViewById(R.id.bill_total_input);
                 EditText tip_input = findViewById(R.id.tip_percent_input);
                 EditText people_input = findViewById(R.id.number_people_input);
 
+                // ensure appropriate bill total input
                 Double entered_bill;
                 if (TextUtils.isEmpty(bill_input.getText().toString())) {
                     bill_input.setError("Bill Total cannot be empty"); // throw error if empty
                     return;
-                } else if (Double.valueOf(bill_input.getText().toString()) <= 0 ) {
-                    bill_input.setError("Bill Total must be greater than 0"); // throw error if empty
+                } else if (Double.valueOf(bill_input.getText().toString()) <= 0) {
+                    bill_input.setError("Bill Total must be greater than 0"); // throw error if less than or each to 0
                     return;
                 } else {
-                    entered_bill = Double.valueOf(bill_input.getText().toString());
+                    entered_bill = Double.valueOf(bill_input.getText().toString()); // else get double value of bill total
                 }
 
+                // ensure appropriate tip percent input
                 double entered_tip;
                 if (TextUtils.isEmpty(tip_input.getText().toString())) {
                     tip_input.setError("Tip Percentage cannot be empty"); // throw error if empty
                     return;
                 } else if (Double.valueOf(tip_input.getText().toString()) < 1 || Double.valueOf(tip_input.getText().toString()) > 100) {
-                    tip_input.setError("Tip Percentage must be between 1 and 100"); // throw error if empty
+                    tip_input.setError("Tip Percentage must be between 1 and 100"); // throw error if less than 1 or greater than 100
                     return;
                 } else {
-                    entered_tip = Double.valueOf(tip_input.getText().toString());
+                    entered_tip = Double.valueOf(tip_input.getText().toString()); // else get double value of tip percentage
                 }
 
+                // ensure appropriate number of people input
                 int entered_people;
                 if (TextUtils.isEmpty(people_input.getText().toString())) {
                     people_input.setError("Number of People cannot be empty"); // throw error if empty
                     return;
                 } else if (Integer.valueOf(people_input.getText().toString()) < 1) {
-                    people_input.setError("Number of People must greater than or equal to 1"); // throw error if empty
+                    people_input.setError("Number of People must greater than or equal to 1"); // throw error if less than 1
                     return;
                 } else {
-                    entered_people = Integer.valueOf(people_input.getText().toString());
+                    entered_people = Integer.valueOf(people_input.getText().toString()); // else get int value of number of people
                 }
 
+                // write bill total, tip percentage, and total number of people to log
                 Log.v("myApp", "entered bill total " + entered_bill);
                 Log.v("myApp", "entered tip percentage  " + entered_tip);
                 Log.v("myApp", "entered number of people  " + entered_people);
 
+                // calculate unadjusted tip
                 tip_per_person_unadjusted = (entered_bill * entered_tip) / (entered_people * 100);
 
+                // write calculated unadjusted tip to log
                 Log.v("myApp", "unadjusted tip per person  " + tip_per_person_unadjusted);
 
+                // if there are 6 or more people, add 18% gratuity to bill total and adjust tip
                 if (entered_people >= 6) {
 
+                    // write adjustment to log
                     Log.v("myApp", "greater than 6 people, adding 18% gratuity");
 
+                    // add 18% to entered bill
                     entered_bill = entered_bill + (entered_bill * .18); // add 18% gratuity
 
+                    // write adjusted bill total to log
                     Log.v("myApp", "new bill total " + entered_bill);
 
+                    // calculate adjusted tip per person
                     tip_per_person_adjusted = (entered_bill * entered_tip) / (entered_people * 100);
 
+                    // write adjusted to to log
                     Log.v("myApp", "adjusted tip per person  " + tip_per_person_adjusted);
                 } else {
-
-                    tip_per_person_adjusted = (entered_bill * entered_tip) / (entered_people * 100);
+                    // else adjusted tip is unadjusted tip
+                    tip_per_person_adjusted = tip_per_person_unadjusted;
 
                     Log.v("myApp", "adjusted tip per person  " + tip_per_person_adjusted);
 
                 }
 
+                // new instance of intent
                 Intent intent = new Intent();
 
+                // target result activity
                 intent.setClass(Calculator.this, Result.class);
 
+                // create new bundle, add adjusted and unadjusted tip per person
                 Bundle bundle = new Bundle();
                 bundle.putDouble("tip_unadjusted", tip_per_person_unadjusted);
                 bundle.putDouble("tip_adjusted", tip_per_person_adjusted);
 
-
+                // add bundle to intent
                 intent.putExtras(bundle);
+                //call result activity
                 startActivity(intent);
 
             }
--- TipsCalculator/app/src/main/java/com/example/tipscalculator/Result.java
+++ TipsCalculator/app/src/main/java/com/example/tipscalculator/Result.java
@@ -8,39 +8,42 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import java.text.DecimalFormat;
 
 
 public class Result extends AppCompatActivity {
 
-
+    //define decimal format to display tips in dollars and cents
     private static DecimalFormat df = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        Button reset_button = findViewById(R.id.reset_button);
+        Button reset_button = findViewById(R.id.reset_button); // get reset button
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Bundle bundle = this.getIntent().getExtras();
+        Bundle bundle = this.getIntent().getExtras(); // get bundle from Calculator activity
 
+        // get tip adjusted and unadjusted from bundle
         Double adjusted_tip = bundle.getDouble("tip_adjusted");
         Double unadjusted_tip = bundle.getDouble("tip_unadjusted");
 
-
+        // display adjusted tip formatted with dollar sign
         TextView adjusted_tip_msg = findViewById(R.id.tip_with_adjustment_value);
         String rounded_adjusted = df.format(adjusted_tip);
         String adjusted_tip_string = "$" + rounded_adjusted + " per person";
         adjusted_tip_msg.setText(adjusted_tip_string);
 
+        // display unadjusted tip formatted with dollar sign
         TextView unadjusted_tip_msg = findViewById(R.id.tip_without_adjustment_value);
         String rounded_unadjusted = df.format(unadjusted_tip);
         String unadjusted_tip_string = "$" + rounded_unadjusted + " per person";
         unadjusted_tip_msg.setText(unadjusted_tip_string);
 
-
+        // on reset call calculator activity
         reset_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
