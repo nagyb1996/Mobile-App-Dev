f17ceb3e7e2bcb9a930a7cfe504f9799a6c129f1
nagyb1996 <nagyb@wit.edu>
2019-06-25 08:26:56 -0400

    Added Lab 8

---
+++ Lab_8_Assignment_3/app/src/main/java/edu/wit/mobileapp/lab_8_assignment_3/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.wit.mobileapp.lab_8_assignment_3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
